d20de5e676104b518dd29343d778b3771e722e7e
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-14 17:15:58 +0300

    initial

--- src/debounce.js
+++ src/debounce.js
@@ -1,37 +1,22 @@
 'use strict';
 
-/**
- * Implement decorator debounce
- *
- * Чтобы оригинальная функция запускалась только после определённой паузы
- * в запусках обёртки, пока пользователь печататет - ничего
- * Если остановился и подождал секунду - вывести последнее значение
- * Функция onChange должна получать тот же `this` и аргументы, что и обёртка
- **/
-function debounce(func, delay) {
-  let timer;
-
-  return function(caller) {
-    clearTimeout(timer);
-
-    const funcCall = () => {
-      return func.call(this, caller);
+/** 
+     * Реализовать декоратор debounce
+     * 
+     * Чтобы оригинальная функция запускалась только после определённой паузы в запусках обёртки
+     * Пока пользователь печататет - ничего
+     * Если остановился и подождал секунду - вывести последнее значение
+     * Функция onChange должна получать тот же `this` и аргументы, что и обёртка
+     **/
+    function debounce(f, delay) {
+      // ...
     }
 
-    timer = setTimeout(funcCall, delay);
-  };
-}
+    function onChange(event) {
+      console.log(event.target.value);
+      // console.log(this.value); // uncomment if you implemented debounce with correct this
+    }
+    
+    let wrapper = debounce(onChange, 1000);
 
-function onChange(event) {
-  output.innerHTML = event.target.value;
-  // eslint-disable-next-line no-console
-  console.log(this.value);
-}
-
-const wrapper = debounce(onChange, 1000);
-
-const input1 = document.getElementById('input1');
-
-const output = document.querySelector('.output');
-
-input1.addEventListener('input', wrapper);
+    input1.addEventListener('input', onChange);
